f10723c6b0942c184733854e089ebf8fa7d261de
Simone Vergeat <simone.vergeat@gmail.com>
2026-04-20 16:54:06 +0200

    Added sku in shipping packages parcels

--- includes/type/object/class-shipping-package-type.php
+++ includes/type/object/class-shipping-package-type.php
@@ -25,6 +25,7 @@ class Shipping_Package_Type {
 			[
 				'description' => __( 'Shipping package parcel', 'wp-graphql-woocommerce' ),
 				'fields'      => [
+					'sku'    => [ 'type' => 'String' ],
 					'length' => [ 'type' => 'Float' ],
 					'width'  => [ 'type' => 'Float' ],
 					'height' => [ 'type' => 'Float' ],
@@ -89,6 +90,7 @@ class Shipping_Package_Type {
 									continue;
 								}
 
+								$sku = (string) $product->get_sku();
 								$length = (float) wc_format_decimal( $product->get_length() ?: 0 );
 								$width  = (float) wc_format_decimal( $product->get_width() ?: 0 );
 								$height = (float) wc_format_decimal( $product->get_height() ?: 0 );
@@ -96,6 +98,7 @@ class Shipping_Package_Type {
 
 								for ( $i = 0; $i < $qty; $i++ ) {
 									$parcels[] = [
+										'sku' 	 => $sku,
 										'length' => $length,
 										'width'  => $width,
 										'height' => $height,
